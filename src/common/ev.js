c9d0ddd87d57a08888dd60b5ad2512b76c16496a
mo.yu <mo.yu@tendcloud.com>
2018-09-13 15:28:09 +0800

    解决多图层时tooltip无法正常显示问题

---
+++ src/common/ev.js
@@ -0,0 +1,25 @@
+let ev = null 
+let isFind = false
+
+const getEV = () =>{
+	return ev
+}
+
+const setEV = (val) =>{
+	ev = val
+}
+
+const getIsFind = () =>{
+	return isFind
+}
+
+const setIsFind = (val) =>{
+	isFind = val
+}
+
+export default {
+	getEV,
+	setEV,
+	getIsFind,
+	setIsFind
+}
