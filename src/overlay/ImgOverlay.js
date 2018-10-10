9c0b9988d132d6fa217cdbffc6597a6a817be904
mo.yu <mo.yu@tendcloud.com>
2018-10-10 15:27:01 +0800

    更新

--- src/overlay/ImgOverlay.js
+++ src/overlay/ImgOverlay.js
@@ -22,13 +22,6 @@ export default class ImgOverlay extends Parameter {
     setOptionStyle(ops) {
         this._setStyle(this._option, ops);
     }
-    setTooltipIsShow(val) {
-        this.setOptionStyle({
-            tooltip: {
-                show: val
-            }
-        });
-    }
     _setState(val) {
         this._state = val;
         this._eventConfig.onState.call(this, this._state);
