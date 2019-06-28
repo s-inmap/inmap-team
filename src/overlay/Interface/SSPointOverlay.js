c4a71eb16184e7d6c1d27d08b7591f89c9b92929
lhywell <lihuiyin128@163.com>
2019-06-28 14:56:20 +0800

    去掉pointOverlay的this._mouseLayer

--- src/overlay/Interface/SSPointOverlay.js
+++ src/overlay/Interface/SSPointOverlay.js
@@ -2,10 +2,7 @@
  * 点的绘制
  */
 import PointOverlay from '../PointOverlay';
-import {
-    isEmpty,
-    detectmob,
-} from '../../common/Util.js';
+import State from '../../config/OnStateConfig';
 export default class SSPointOverlay extends PointOverlay {
     constructor(opts) {
         super(opts);
@@ -15,6 +12,15 @@ export default class SSPointOverlay extends PointOverlay {
         } else if (mouseOver.show === undefined || mouseOver.show === true) {
             this._mouseOverShow = true;
         }
+
+        this._mouseLayer = null;
+    }
+    setZIndex(zIndex) {
+        this._zIndex = zIndex;
+        if (this._container) this._container.style.zIndex = this._zIndex;
+    }
+    _parameterInit() {
+        this._initLegend();
     }
     _getMpp() {
         let mapCenter = this._map.getCenter();
@@ -23,6 +29,35 @@ export default class SSPointOverlay extends PointOverlay {
         let dpx = Math.abs(this._map.pointToPixel(mapCenter).y - this._map.pointToPixel(cpt).y);
         return this._map.getDistance(mapCenter, cpt) / dpx;
     }
+    _drawMouseLayer() {
+        let overArr = this._overItem ? [this._overItem] : [];
+        if (this.getRenderData().length > 0) {
+            this._loopDraw(this._ctx, this._selectItem.concat(overArr), true);
+        }
+    }
+    _clearAll() {
+        this._clearCanvas();
+    }
+    refresh() {
+        this._setState(State.drawBefore);
+        this._clearCanvas();
+        if (this._batchesData) {
+            this._batchesData.clear();
+            this._batchesData.action(this._workerData, this._loopDraw, this._ctx);
+
+        } else {
+            this._loopDraw(this._ctx, this._workerData, false);
+        }
+        if (this._styleConfig.normal.label.show) {
+            this._drawLabel(this._ctx, this._workerData);
+        }
+        this._drawMouseLayer();
+        this._setState(State.drawAfter);
+    }
+    _Tdispose() {
+        this._batchesData && this._batchesData.clear();
+    }
+
     _tMousemove(event) {
         if (this._eventType == 'onmoving') {
             return;
@@ -59,5 +94,5 @@ export default class SSPointOverlay extends PointOverlay {
             this._eventConfig.onMouseLeave.call(this, this._overItem, event);
         }
     }
-    
+
 }
